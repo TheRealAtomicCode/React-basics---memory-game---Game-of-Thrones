5d275e1c622ed35179dd06061e902890fcd6f449
Atomic Code <81088443+TheRealAtomicCode@users.noreply.github.com>
2022-07-03 16:44:58 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+// import { useState, useRef, useEffect } from 'react'
+import Cards from './Components/Cards'
+
+
+function App() {
+ 
+
+  return (
+    <div className="App">
+      <h1>Game of thrones</h1>
+      <Cards />
+    </div>
+  );
+}
+
+
+export default App;
